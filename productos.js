60184a1c1c07957e864cb4e897c9e417ba39314f
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-04 16:54:46 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,1 +1,19 @@
+const productos = {
+    clasico: {
+        nombre: "Pancho Clásico",
+        precio: 2000,
+        esSuper: false
+    },
 
+    super: {
+        nombre: "Super Pancho",
+        precio: 4500,
+        esSuper: true
+    },
+
+    cheddar: {
+        nombre: "Pancho Cheddar",
+        precio: 6500,
+        esSuper: false
+    }
+};
