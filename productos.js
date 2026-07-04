ad487e1214307ee660132e0112be32ae48f9cb80
lululinnea0-crypto <lululinnea0@gmail.com>
2026-07-04 17:58:52 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -2,18 +2,27 @@ const productos = {
     clasico: {
         nombre: "Pancho Clásico",
         precio: 2000,
+        descripcion: "Salchicha premium, ketchup, mostaza y papas pay.",
+        imagen: "individual-clasico.png",
+        boton: "Pedir",
         esSuper: false
     },
 
     super: {
         nombre: "Super Pancho",
         precio: 4500,
+        descripcion: "Salchicha premium + ¡toppings a elección!",
+        imagen: "individual-super.png",
+        boton: "Personalizar",
         esSuper: true
     },
 
     cheddar: {
         nombre: "Pancho Cheddar",
         precio: 6500,
+        descripcion: "Salchicha premium, abundante cheddar y panceta crocante.",
+        imagen: "individual-cheddar.png",
+        boton: "Pedir",
         esSuper: false
     }
 };
